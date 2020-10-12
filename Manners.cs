852ccda9d82c2bd437f8ecd3eb74460aba8f5131
Hushaby-cloud <13152915010@163.com>
2020-10-12 21:32:26 +0800

    在基本的加减乘除运算成功能基础上，对程序进行重构，当用户选择加法操作时，可以对输入的两个字符串进行连接运算；当用户选择减法操作时，可以从用户输入的第一个字符串中去除第二个字符串。

---
+++ Manners.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_5_2
+{
+    class Manners
+    {
+          string step;
+          string a, b;
+          public void manners()
+          { 
+               try
+               {
+                   Console.WriteLine("请输入第一个操作数");
+                   a = Console.ReadLine();
+                   Console.WriteLine("请输入第二个操作数");
+                   b = Console.ReadLine();
+                   Console.WriteLine("请输入要执行的操作 + - * /");
+                   step = Console.ReadLine();
+                   switch (step)
+                   {
+                       case "+":
+                           {
+                               Add();
+                               break;
+                           }
+                       case "-":
+                           {
+                               Subtraction();
+                               break;
+                           }
+                       case "*":
+                           {
+                               Multiplication();
+                               break;
+                           }
+                       case "/":
+                           {
+                               Division();
+                               break;
+                           }
+                   }
+               }
+               catch
+               {
+                   Console.WriteLine("发生异常，请重启程序");
+               }
+          }
+          private void Add()
+          {
+              bool flag=false;
+              for (int i = 0; i < a.Length; i++)
+              {
+                  if (a[i] < 48 || a[i] > 57)
+                      flag = true;
+              }
+              if (flag)
+                  Console.WriteLine("{0}+{1}={2}",a,b,a+b);
+             else
+              {
+                  float c = float.Parse(a);
+                  float d = float.Parse(b);
+                  Console.WriteLine("{0}+{1}={2}", a, b, c + d);
+              }
+          }
+          private void Subtraction()
+          {
+             bool flag = false;
+              for (int i = 0; i < a.Length; i++)
+              {
+                  if (a[i] < 48 || a[i] > 57)
+                      flag = true;
+              }
+              if (flag == true)
+              {
+                  string c="";
+                  for (int i = 0; i < a.Length; i++)
+                  {
+                      if (a[i] == b[0])
+                      {
+                        for (int j = 0; j < a.Length - b.Length; j++)
+                        {
+                            if (j < i)
+                                c += a[j];
+                        }
+                        for (int k = i + b.Length; k < a.Length; k++)
+                        {
+                            c += a[k];
+                        }
+                    }
+                }
+                  Console.WriteLine("{0}-{1}={2}", a, b, c);
+              }
+              else
+              {
+                  float c = float.Parse(a);
+                  float d = float.Parse(b);
+                  Console.WriteLine("{0}-{1}={2}", a, b, c - d);
+              }
+          }
+          private void Multiplication()
+          {
+              float c = float.Parse(a);
+              float d = float.Parse(b);
+              Console.WriteLine("{0}*{1}={2}", a, b, c*d);
+          }
+          private void Division()
+          {
+              float c = float.Parse(a);
+              float d = float.Parse(b);
+              Console.WriteLine("{0}/{1}={2}", a, b, c/d);
+          }
+            public void  Equals() //重载equals
+          {
+              if (a == b)
+                  Console.WriteLine("a=b");
+              else
+                  Console.WriteLine("a!=b");
+          }
+               
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manners m = new Manners();
+            m.manners();
+            m.Equals();
+        }
+    }
+}
